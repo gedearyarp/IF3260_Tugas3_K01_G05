ec367e08375399e1a897d34be08d6e8837ad45c7
I Gede Arya Raditya Parameswara <71829426+gedearyarp@users.noreply.github.com>
2023-04-02 13:45:35 +0700

    feat: update vert & frag code

--- src/util/shader-generator.js
+++ src/util/shader-generator.js
@@ -1,54 +1,112 @@
+// reference: https://webglfundamentals.org/webgl/lessons/webgl-environment-maps.html
+// reference: https://webglfundamentals.org/webgl/lessons/webgl-cube-maps.html
+// reference: https://apoorvaj.io/exploring-bump-mapping-with-webgl/#normal-mapping
+
 const vertCode3D = `
     attribute vec3 aPosition;
-    attribute vec2 aTexcoord;
     attribute vec3 aNormal;
+    attribute vec3 aColor;
+    attribute vec3 aTangent;
+    attribute vec3 aBitangent;
+    attribute vec2 aTexCoord;
 
-    uniform float fudgeFactor;
-    uniform mat4 mTransform;
-    uniform mat4 mProjection;
+    uniform mat4 uProjection;
+    uniform mat4 uView;
+    uniform mat4 uWorld;
+    uniform mat4 uNormal;
 
-    varying float vColor;
-    varying vec2 vTexcoord;
+    varying vec3 vWorldPosition;
+    varying vec3 vWorldNormal;
+    varying vec3 vColor;
+    varying vec2 vTexCoord;
 
-    void main(void) {
-        vec4 transformedPos = mTransform * vec4(aPosition.xy, aPosition.z * -1.0, 1.0);
-        vec4 projectedPos   = mProjection * transformedPos;
+    varying vec3 vTsLightPos;
+    varying vec3 vTsFragPos;
 
-        vColor = min(max((1.0 - transformedPos.z) / 2.0, 0.0), 1.0);
-        vTexcoord = aTexcoord;
+    mat3 transpose(in mat3 inMatrix) {
+        vec3 i0 = inMatrix[0];
+        vec3 i1 = inMatrix[1];
+        vec3 i2 = inMatrix[2];
 
-        if (fudgeFactor < 0.5) gl_Position = projectedPos;
-        else {
-            float zDivider = 1.5 + projectedPos.z * fudgeFactor;
-            gl_Position = vec4(projectedPos.xy / zDivider, projectedPos.zw);
+        mat3 outMatrix = mat3(
+            vec3(i0.x, i1.x, i2.x),
+            vec3(i0.y, i1.y, i2.y),
+            vec3(i0.z, i1.z, i2.z)
+        );
+
+        return outMatrix;
+    }
+
+    void main() {
+        gl_Position = uProjection * uView * uWorld * vec4(aPosition, 1.0);
+        vWorldPosition = (uWorld * vec4(aPosition, 1.0)).xyz;
+        vWorldNormal = mat3(uWorld) * aNormal;
+
+        vColor = aColor;
+        vTexCoord = aTexCoord;
+
+        vTsFragPos = (uWorld * vec4(aPosition, 1.0)).xyz;
+        vec3 t = normalize((uNormal * vec4(aTangent, 0.0)).xyz);
+        vec3 b = normalize((uNormal * vec4(aBitangent, 0.0)).xyz);
+        vec3 n = normalize((uNormal * vec4(cross(aTangent, aBitangent), 0.0)).xyz);
+
+        mat3 tbn = transpose(mat3(t, b, n));
+        vTsLightPos = tbn * vec3(0.0, 0.0, 1.0);
+        vTsFragPos = tbn * vTsFragPos;
+    }
+`
+
+const fragCode3D = `
+    precision highp float;
+
+    varying vec3 vWorldPosition;
+    varying vec3 vWorldNormal;
+    varying vec3 vColor;
+    varying vec2 vTexCoord;
+
+    varying vec3 vTsLightPos;
+    varying vec3 vTsFragPos;
+
+    uniform samplerCube uTextureReflective;
+    uniform samplerCube uTextureCubeMap;
+    uniform sampler2D uTextureBump;
+
+    uniform vec3 uWorldCameraPosition;
+    uniform bool uUseShading;
+    uniform int uTextureType;
+
+    void main() {
+        vec3 worldNormal = normalize(vWorldNormal);
+
+        gl_FragColor = vec4(vColor, 1.0);
+
+        if (uTextureType == 0) { // bump
+            vec3 lightDirection = normalize(vTsLightPos - vTsFragPos);
+            vec3 albedo = texture2D(uTextureBump, vTexCoord).rgb;
+            vec3 ambient = 0.3 * albedo;
+
+            vec3 norm = normalize(texture2D(uTextureBump, vTexCoord).rgb * 2.0 - 1.0);
+            float diffuse = max(dot(lightDirection, norm), 0.0);
+
+            gl_FragColor = vec4(ambient + diffuse * albedo, 1.0);
+        } else if (uTextureType == 1) { // reflective
+            vec3 worldCameraToVertex = normalize(vWorldPosition - uWorldCameraPosition);
+            vec3 reflectVector = reflect(worldCameraToVertex, worldNormal);
+
+            vec4 color = textureCube(uTextureReflective, reflectVector);
+            gl_FragColor = color;
+        } else if (uTextureType == 2) { // cube map
+            gl_FragColor = texture2D(uTextureCubeMap, vTexCoord);
+        }
+
+        if (uUseShading) {
+            vec3 lightDirection = normalize(vec3(0.0, 0.0, 1.0));
+            float light = max(dot(worldNormal, lightDirection), 0.0);
+            gl_FragColor = vec4(gl_FragColor.rgb * light, gl_FragColor.a);
         }
     }
 `
 
-const fragCode3D = {
-    light: `
-        precision mediump float;
-        uniform vec3 uColor;
-        uniform sampler2D uTexture;
-        uniform bool uUseTexture;
-
-        varying float vColor;
-        varying vec2 vTexcoord;
-
-        void main(void) {
-            if (uUseTexture) gl_FragColor = texture2D(uTexture, vTexcoord);
-            else gl_FragColor = vec4(uColor, 1.0);
-        }
-    `,
-    flat: `
-        precision mediump float;
-        uniform vec3 uColor;
-
-        void main(void) {
-            gl_FragColor = vec4(uColor, 1.0);
-        }
-    `
-}
 
 function generateShaderProgram (gl, vertCode, fragCode) {
     let vertShader = gl.createShader(gl.VERTEX_SHADER);
