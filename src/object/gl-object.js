c64a60d9fa6f2769896eef47b2bbb2181f5f2f14
I Gede Arya Raditya Parameswara <71829426+gedearyarp@users.noreply.github.com>
2023-04-11 21:16:49 +0700

    fix: bug on webgl

--- src/object/gl-object.js
+++ src/object/gl-object.js
@@ -190,13 +190,13 @@ export class GlObject {
         
             const level = 0;
             const internalFormat = gl.RGBA;
-            const width = 1;
-            const height = 1;
+            const width = 512;
+            const height = 512;
             const border = 0;
             const srcFormat = gl.RGBA;
             const srcType = gl.UNSIGNED_BYTE;
-            const pixel = new Uint8Array([0, 0, 255, 255]);
-            gl.texImage2D(target, level, internalFormat, width, height, border, srcFormat, srcType, pixel);
+
+            gl.texImage2D(target, level, internalFormat, width, height, border, srcFormat, srcType, null);
 
             const image = new Image();
             image.src = url;
